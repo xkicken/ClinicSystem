7ecd2cf0bb933a64a3d4aba8a3bf6f474071e2e3
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-04-16 17:12:35 +1200

    refactor: change project name

--- ClinicSystem/settings.py
+++ ClinicSystem/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'AppointmentManagementSystem.urls'
+ROOT_URLCONF = 'ClinicSystem.urls'
 
 TEMPLATES = [
     {
