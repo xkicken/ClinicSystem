5ab9389d20e1ad36b5b42b3b169245b74c75f14f
xkicken <61647663+xkicken@users.noreply.github.com>
2026-04-23 14:51:44 +1200

    wip-transfer

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Appointment, Patient, Doctor, Specialty, TimeSlot
 # Register your models here.
+admin.site.register(Appointment)
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Specialty)
+admin.site.register(TimeSlot)
--- appointment/models.py
+++ appointment/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-class Specialty(models.Model):
-    specialtyID = models.AutoField(primary_key=True)
-    specialtyName = models.CharField(max_length=255)
 
-class Role(models.Model):
-    roleID = models.AutoField(primary_key=True)
-    roleName = models.CharField(max_length=255)
+#Core Tables
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Patient(models.Model):
-    patientID = models.AutoField(primary_key=True)
     firstName = models.CharField(max_length=150)
     lastName = models.CharField(max_length=150)
     email = models.EmailField()
@@ -19,24 +19,46 @@ class Patient(models.Model):
     emergencyContact = models.CharField(max_length=255)
     emergencyContactPhone = models.CharField(max_length=15)
 
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
+
 class Doctor(models.Model):
-    doctorID = models.AutoField(primary_key=True)
     firstName = models.CharField(max_length=150)
     lastName = models.CharField(max_length=150)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
 
-class Appointment(models.Model):
-    appointmentID = models.AutoField(primary_key=True)
-    appointmentDate = models.DateField()
-    appointmentTime = models.TimeField()
-    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    doctorID = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    appointmentStatus = models.CharField(max_length=255)
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
 
-class User(models.Model):
-    username = models.CharField(max_length=150)
-    password = models.CharField(max_length=255)
-    email = models.EmailField()
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+class TimeSlot(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateField()
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.doctor} {self.date} {self.startTime} - {self.endTime}"
+
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    timeSlot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+
+    STATUS_CHOICES = [
+        ('BOOKED', 'Booked'),
+        ('CANCELLED', 'Cancelled'),
+        ('DONE', 'Completed')
+    ]
+    appointmentStatus = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='BOOKED'
+    )
+
+    def __str__(self):
+        return f"{self.patient} {self.timeSlot.startTime} - {self.timeSlot.endTime}"
