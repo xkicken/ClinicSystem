c3ba99ee16c6f9553a60ed08fe3170723e163414
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-04-23 16:21:56 +1200

    Refactor: Updated admin.py include new models

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Appointment, Patient, Doctor, Specialty, TimeSlot
-# Register your models here.
-admin.site.register(Appointment)
+from .models import Specialty, Patient, Doctor, TimeSlot, Appointment
+admin.site.register(Specialty)
 admin.site.register(Patient)
 admin.site.register(Doctor)
-admin.site.register(Specialty)
 admin.site.register(TimeSlot)
+admin.site.register(Appointment)
+# Register your models here.
