9cfff6b1b93385174db52b16fa89fbe7fa5a5f70
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-04-28 19:11:54 +1200

    feat: added base template - added nav bar in base template
    - added staticfiles_dirs in settings.py
    - created dir for static content

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
+def home(request):
+    doctors = Doctor.objects.select_related('specialty', 'account').all()
+    return render(request, 'appointment/home.html', {'doctors': doctors})
+
+def profile(request):
+    return render(request, 'appointment/profile.html')
 # Create your views here.
+def doctor_dashboard(request):
+    return render(request, 'appointment/doctor_dashboard.html')
+def patient_dashboard(request):
+    return render(request, 'appointment/patient_dashboard.html')
---
+++ static/appointment/js/base.js
@@ -0,0 +1,5 @@
+function toggleTheme() {
+  const html = document.documentElement;
+  const current = html.getAttribute("data-bs-theme");
+  html.setAttribute("data-bs-theme", current === "dark" ? "light" : "dark");
+}
