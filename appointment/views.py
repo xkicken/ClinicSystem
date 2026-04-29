cc3bf44b361c58215622255cd630cf53604ce282
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-04-29 17:38:03 +1200

    feat: added calendar views to added array to template from db for full calendar to use.
    - check if user is authenticated
    - filter data based on user or doctor

--- appointment/views.py
+++ appointment/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 def home(request):
@@ -12,5 +13,51 @@ def doctor_dashboard(request):
     return render(request, 'appointment/doctor_dashboard.html')
 def patient_dashboard(request):
     return render(request, 'appointment/patient_dashboard.html')
+
+@login_required
 def calendar(request):
-    return render(request, 'appointment/calendar.html')
+    user = request.user
+
+    group = user.groups.first().name if user.groups.exists() else None
+    if group == "Doctor":
+        appointments = Appointment.objects.select_related(
+            'patient', 'time_slot', 'time_slot__doctor'
+        ).filter(time_slot__doctor_id=user.id, appointment_status='BOOKED')
+
+        events = []
+        for apt in appointments:
+            events.append({
+                'id': apt.id,
+                'title': f"{apt.patient.first_name} {apt.patient.last_name}",
+                'start': f"{apt.time_slot.date}T{apt.time_slot.start_time}",
+                'end': f"{apt.time_slot.date}T{apt.time_slot.end_time}",
+            })
+
+        return render(request, "appointment/calendar.html", {
+            "data": events
+        })
+    elif group == "User":
+        patients = Patient.objects.filter(account_id=user.id)
+
+        events = []
+
+        for p in patients:
+            appointments = Appointment.objects.select_related(
+                'time_slot', 'time_slot__doctor'
+            ).filter(patient_id=p.id)
+
+            for apt in appointments:
+                events.append({
+                    'id': apt.id,
+                    'title': f"{p.first_name} {p.last_name}",
+                    'start': f"{apt.time_slot.date}T{apt.time_slot.start_time}",
+                    'end': f"{apt.time_slot.date}T{apt.time_slot.end_time}",
+                })
+
+        return render(request, "appointment/calendar.html", {
+            "data": events
+        })
+
+    return render(request, "appointment/calendar.html")
+
+
