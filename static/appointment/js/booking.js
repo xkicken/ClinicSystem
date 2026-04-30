c622ee6fc08471b0692bc52d86efc8a6d29dde6c
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-04-30 19:23:37 +1200

    feat: calendar page changes - change calendar to only show time between 9am and 5pm - change each slot the calendar to show 30mins - add a hover effect when hovering over a timeslot to let user know to select it - added a confirmation popup

--- static/appointment/js/booking.js
+++ static/appointment/js/booking.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const timeslots = JSON.parse(document.getElementById('time_slots_available').textContent);
     console.log(timeslots);
-    
+
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'timeGridWeek',
+        slotMinTime: '09:00:00',
+        slotMaxTime: '17:00:00',
+        allDaySlot: false,
+        eventDisplay: 'block',
+        eventOverlap: false,
+        slotDuration: '00:10:00',
 
         headerToolbar: {
             left: 'prev,next today',
@@ -19,7 +25,22 @@ document.addEventListener('DOMContentLoaded', function () {
             listWeek: {buttonText: 'List'}
         },
 
+        eventClick: function (info) {
+            let confirmBook = confirm(
+                "Do you want to book this time slot?\n" +
+                "Time: " + info.event.start.toLocaleString()
+            );
+
+            if (!confirmBook) return;
+
+            let doctor = document.getElementById('doctorSelect').value;
+
+            window.location.href = `/appointment/booking?doctor=${doctor}&start_date=${info.event.start.toLocaleString()}`;
+
+        },
+
         events: timeslots
     });
     calendar.render();
 });
+
