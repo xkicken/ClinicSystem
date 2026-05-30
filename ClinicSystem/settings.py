27b2c6db91d5c810b37231d8906ee6260bc47ddb
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-05-30 16:05:28 +1200

    feat: change auth method to JWT token

--- ClinicSystem/settings.py
+++ ClinicSystem/settings.py
@@ -138,4 +138,15 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
