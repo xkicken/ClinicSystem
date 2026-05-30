d4803f9172e2a71787f6e385154652a3a20e8a0b
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-05-31 08:50:03 +1200

    feat: added login and token refresh endpoint using simplejwt plugin

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('auth/register', views.RegisterView.as_view()),
+    path('auth/login', TokenObtainPairView.as_view()),
+    path('auth/refresh', TokenRefreshView.as_view()),
 ]
