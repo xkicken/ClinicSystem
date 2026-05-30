8ef14770e2cc404afcc7d6ea0b39652c040f6960
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-05-30 16:06:17 +1200

    feat: added register method

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getdata, name="gatDate"),
+    path('auth/register', views.RegisterView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,25 @@
+from django.contrib.auth.models import User, Group
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-@api_view(['GET'])
-def getdata(request):
-    return Response("routes")
+from .serializers import *
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user_group, _ = Group.objects.get_or_create(name='User')
+            user.groups.add(user_group)
+            UserProfile.objects.create(user=user)
+            return Response({
+                'detail': 'Registered successfully.'
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            'detail': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
