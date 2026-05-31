79c7a28b636b9d92090b8229c239f5cd5ca64814
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-05-31 18:06:45 +1200

    feat: added login page to react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import UserDashboard from './pages/UserDashboard'
 import Navbar from './navbar'
+import Login from './pages/Login'
 
 export default function App() {
-  return (
-    <BrowserRouter>
-        <Navbar></Navbar>
-      <Routes>
-        <Route path="/dashboard" element={<UserDashboard />} />
-      </Routes>
+    return (
+        <BrowserRouter>
+            <Navbar></Navbar>
+            <Routes>
+                <Route path="/dashboard" element={<UserDashboard/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
         </BrowserRouter>
-  )
+    )
 }
