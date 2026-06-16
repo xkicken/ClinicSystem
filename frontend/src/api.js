7823b1d8674bbbd5a9bcef6a6d934d75b97cf21a
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-06-17 03:01:07 +1200

    chore: deleted files as its not used

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "http://localhost:8000/api";
-
-export async function apiFetch(path, options = {}) {
-    return await fetch(`${BASE_URL}${path}`, {
-        headers: {"Content-Type": "application/json"},
-        credentials: "include",
-        ...options,
-    });
-}
