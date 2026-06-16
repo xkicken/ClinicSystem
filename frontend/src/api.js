2c6e95cb4d630a0cc8eb302c1209425cab4c1b2d
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-06-16 19:18:58 +1200

    feat: updated to use jwt cookie token and use local host ip

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
-const BASE_URL = "http://127.0.0.1:8000/api";
+const BASE_URL = "http://localhost:8000/api";
 
 export async function apiFetch(path, options = {}) {
     return await fetch(`${BASE_URL}${path}`, {
-      headers: {"Content-Type": "application/json"},
-      ...options,
-  });
+        headers: {"Content-Type": "application/json"},
+        credentials: "include",
+        ...options,
+    });
 }
