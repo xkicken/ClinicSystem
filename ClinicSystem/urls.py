b577c8db06b0463500c772a6c368eb1dcb589626
Antony Lin <61647663+xkicken@users.noreply.github.com>
2026-06-19 18:52:37 +1200

    build: updated url paths for django templates

--- ClinicSystem/urls.py
+++ ClinicSystem/urls.py
@@ -18,32 +18,25 @@ from django.contrib import admin
 from django.urls import path, include
 from appointment.views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('home/', home, name='home'),
-    path('register/', register, name='register'),
-
-    path('user_dashboard/', user_dashboard, name='user_dashboard'),
-    path("hijack/", include("hijack.urls")),
-    path('accounts/', include('django.contrib.auth.urls')),
-
-    # patient
-    path('calendar/', calendar, name='calendar'),
-    path('booking/', booking, name='booking'),
-    path('booking_confirm/', booking_confirm, name='booking_confirm'),
-    path('patient/<int:id>/', patient_view, name='patient_view'),
-    path('booking/view/<int:id>/', booking_view, name='booking_view'),
-    path('profile/<int:id>/', profile, name='profile'),
-    path('add_patient/', add_patient, name='add_patient'),
-    path('delete_patient/<int:id>/', delete_patient, name='delete_patient'),
-
-    # doctor
-    path('doctor_dashboard/', doctor_dashboard, name='doctor_dashboard'),
-
-    # admin
-    path('admin_dashboard/', admin_dashboard, name='admin_dashboard'),
-    path('doctor/<int:id>/', doctor_view, name='doctor_view'),
-    path('delete_doctor/<int:id>/', delete_doctor, name='delete_doctor'),
-    path('add_doctor/', add_doctor, name='add_doctor'),
-    path('api/', include('api.urls'))
+    path('/django/admin/', admin.site.urls),
+    path('/django/accounts/', include('django.contrib.auth.urls')),
+    path('/django/home/', home, name='home'),
+    path('/django/register/', register, name='register'),
+    path('/django/user_dashboard/', user_dashboard, name='user_dashboard'),
+    path("/django/hijack/", include("hijack.urls")),
+    path('/django/accounts/', include('django.contrib.auth.urls')),
+    path('/django/calendar/', calendar, name='calendar'),
+    path('/django/booking/', booking, name='booking'),
+    path('/django/booking_confirm/', booking_confirm, name='booking_confirm'),
+    path('/django/patient/<int:id>/', patient_view, name='patient_view'),
+    path('/django/booking/view/<int:id>/', booking_view, name='booking_view'),
+    path('/django/profile/<int:id>/', profile, name='profile'),
+    path('/django/add_patient/', add_patient, name='add_patient'),
+    path('/django/delete_patient/<int:id>/', delete_patient, name='delete_patient'),
+    path('/django/doctor_dashboard/', doctor_dashboard, name='doctor_dashboard'),
+    path('/django/admin_dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('/django/doctor/<int:id>/', doctor_view, name='doctor_view'),
+    path('/django/delete_doctor/<int:id>/', delete_doctor, name='delete_doctor'),
+    path('/django/add_doctor/', add_doctor, name='add_doctor'),
+    path('/api/', include('api.urls'))
 ]
